b93ad4f77f44f2b168b5a89bca743d45dd92fd60
Behrooz <akbari_behrooz@yahoo.com>
2022-11-15 06:47:48 +0300

    Add operate func and modify other functions
    also added event listeners for adding decimals
    and the negative sign.

--- script.js
+++ script.js
@@ -1,49 +1,126 @@
-function add(a, b, ...args) {
-    let sum = a + b;
-    args.forEach(arg => sum += arg);
-    console.log(sum);
+function add([...args]) {
+    let result = 0;
+    args.forEach(arg => result += arg);
+    return result;
 }
-// add(1, 10, 10.5, 3);
-
-function subtract(a, b, ...args) {
-    let sum = a - b;
-    for (i = 0; i < args.length; i++) {
-        sum -= args[i];
-    }
-    console.log(sum);
+function subtract([a, b = 0, ...args]) {
+    let result = a - b;
+    args.forEach(arg => result += arg);
+    return result;
 }
-// subtract(10, 2, 5, 6);
-
-function multiply(a, b, ...array) {
-    let result = a * b;
+function multiply([...array]) {
+    let result = 1;
     for (let num of array) {
         result *= num;
     }
-    console.log(result);
+    return result;
 }
-// multiply(-1.5, 10, 2, -0.1, 2.33);
-
-function divide(a, b, ...numbers) {
+function divide([a, b = 1, ...args]) {
     let result = a / b;
-    numbers.forEach(number => result /= number);
-    console.log(result);
+    for (i = 0; i < args.length; i++) result /= args[i];
+    return result;
 }
-// divide(39, 6, 0.5, 1/3);
+
+// ===================================================================
+const display = document.querySelector('div.display');
+const numberBtn = document.querySelectorAll('div.number');
+const opBtn = document.querySelectorAll('div.operator');
+
+const plusBtn = document.querySelector('#plus');
+const minusBtn = document.querySelector('#minus');
+const crossBtn = document.querySelector('#cross');
+const divBtn = document.querySelector('#divide');
+const equalBtn = document.querySelector('#equal');
+const clearBtn = document.querySelector('#clear');
+const decimalBtn = document.querySelector('div.decimal');
+const posNegBtn = document.querySelector('div.pos-neg');
+
+// ===================================================================
+let operation = {inputs: [], sign: ''};
+let inputs = operation.inputs;
+let sign = operation.sign;
+let disp = display.dataset;
 
 function operate() {
-    let a = prompt('enter first number(a) a ? b');
-    let operator = prompt(`choose math operator(?) ${a} ? b`);
-    let b = prompt(`enter second number(b) ${a}${operator} b`);
-
-    if (operator == '+') add(+a, +b);
-    if (operator == '-') subtract(a, b);
-    if (operator == '*') multiply(a, b);
-    if (operator == '/') divide(a, b);
+    if (disp.input) inputs.push(+disp.input);
+    if (sign == 'plus') {
+        inputs = [add(inputs)];
+        display.textContent = add(inputs);
+    } else if (sign == 'minus') {
+        inputs = [subtract(inputs)];
+        display.textContent = subtract(inputs);
+    } else if (sign == 'cross') {
+        inputs = [multiply(inputs)];
+        display.textContent = multiply(inputs);
+    } else if (sign == 'divide') {
+        inputs = [divide(inputs)];
+        display.textContent = divide(inputs);
+    }
+    // if (display.textContent == 'Infinity') display.textContent = 'stop it!';
+    sign = '';
+    disp.input = '';
 }
-// operate()
 
-const display = document.querySelector('div.display');
+// ===================  PUT NUMBERS TO DISPLAY  ======================
 display.textContent = '0';
+numberBtn.forEach(button => button.addEventListener('click', () => {
+    if (sign == 'equal') {
+        inputs = [];
+        sign = '';
+    }
+    disp.input += button.classList[1];
+    display.textContent = disp.input;
+}))
 
-const numberBtn = document.querySelectorAll('div.number');
-numberBtn.forEach(button => button.addEventListener('click', () => display.textContent += button.classList[1]));
+// ================  LET INPUT DECIMALS PROPERLY  ====================
+decimalBtn.addEventListener('click', () => {
+    if (disp.input == '') {
+        disp.input += '0.';
+        display.textContent = disp.input;
+    }
+    if (!disp.input.includes('.')) {
+        disp.input += '.';
+        display.textContent = disp.input;
+}})
+
+// =======================  TOGGLE POS/NEG  ==========================
+posNegBtn.addEventListener('click', () => {
+    if (inputs == '' && !disp.input) return;
+    if (disp.input) {
+        disp.input *= -1;
+        display.textContent = disp.input;
+    }
+    if (inputs != '') {
+        inputs[0] *= -1;
+        display.textContent = inputs[0];
+    }
+})
+
+// =========================    + - * /    =========================== 
+opBtn.forEach(button => button.addEventListener('click', () => {
+    if (sign) operate();
+    if (disp.input) inputs.push(+disp.input);
+    disp.input = '';
+    sign = button.id;
+
+    console.log(inputs);
+    console.log(sign);
+}))
+
+// ==========================    = = =    ============================
+equalBtn.addEventListener('click', () => {
+    if (disp.input) operate();
+    sign = 'equal';
+
+    console.log(inputs);
+    console.log(sign);
+})
+
+// ==========================    c c c    ============================
+clearBtn.addEventListener('click', () => {
+    // document.location.reload()
+    disp.input = '';
+    display.textContent = '0';
+    inputs = [];
+    sign = '';
+})
